d96981701965675e0281cc3904eeb819da94d752
Justin Byrne <theskaterjesus@gmail.com>
2022-01-22 16:28:17 +0000

    Created Category model, factory and migration

---
+++ app/Models/Category.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Category extends Model
+{
+    use HasFactory, HasSlug;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+}
