a839ae09e960e1c5e62c6e54f984bd0470673da2
Justin Byrne <theskaterjesus@gmail.com>
2022-01-23 22:08:34 +0000

    Created controllers for Orders, Products and Categories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Category $categories)
+    {
+        //
+    }
+
+    public function edit(Category $categories)
+    {
+        //
+    }
+
+    public function update(Request $request, Category $categories)
+    {
+        //
+    }
+
+    public function destroy(Category $categories)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Product $products)
+    {
+        //
+    }
+
+    public function edit(Product $products)
+    {
+        //
+    }
+
+    public function update(Request $request, Product $products)
+    {
+        //
+    }
+
+    public function destroy(Product $products)
+    {
+        //
+    }
+}
