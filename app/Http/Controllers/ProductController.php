7e43e1ef20bd962d670c4e64d0fc65aa75207657
Justin Byrne <theskaterjesus@gmail.com>
2022-01-24 20:34:03 +0000

    Added functions to product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,42 +3,82 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Category;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\RedirectResponse;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        //
+        $products = Product::all();
+
+        return view('products.index', compact('products'));
     }
 
-    public function create()
+    public function create(): View
     {
-        //
+        $categories = Category::all(['id', 'name']);
+        
+        return view('products.create', compact('categories'));
     }
 
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'title' => 'required|string|unique:App\Models\Product,title',
+            'description' => 'required|string',
+            'price' => 'required|integer',
+            'category_ids' => 'required|array|exists:App\Models\Category,id'
+        ]);
+
+        $product = Product::create($validated);
+        $product->categories()->sync($request->input('category_ids'));
+
+        return redirect()->route('products.index');
     }
 
-    public function show(Product $products)
+    public function show(Product $product): View
     {
-        //
+        $product->load('categories');
+        
+        return view('products.show', compact('product'));
     }
 
-    public function edit(Product $products)
+    public function edit(Product $product)
     {
-        //
+        $product->load('categories');
+
+        $categories = Category::all(['id', 'name']);
+        
+        return view('products.edit', compact('produc', 'categories'));
     }
 
-    public function update(Request $request, Product $products)
+    public function update(Request $request, Product $product): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'title' => [
+                'required',
+                'string',
+                Rule::unique('Product')->ignore($product->id),
+            ],
+            'description' => 'required|string',
+            'price' => 'required|integer',
+            'category_ids' => 'required|array|exists:App\Models\Category,id'
+        ]);
+
+        $product->update($validated);
+        $product->categories()->sync($request->input('category_ids'));
+
+        return redirect()->route('products.index');
     }
 
-    public function destroy(Product $products)
+    public function destroy(Product $product): RedirectResponse
     {
-        //
+        $product->delete();
+
+        return redirect()->route('products.index');
     }
 }
