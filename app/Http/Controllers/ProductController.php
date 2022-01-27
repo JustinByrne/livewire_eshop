d4009ac81aea1e2980fd54c20f50c5cde34f5736
Justin Byrne <theskaterjesus@gmail.com>
2022-01-27 21:26:52 +0000

    Added categories to the filter

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,8 +14,9 @@ class ProductController extends Controller
     public function index(): View
     {
         $products = Product::all();
+        $categories = Category::all();
 
-        return view('products.index', compact('products'));
+        return view('products.index', compact('products', 'categories'));
     }
 
     public function create(): View
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -12,7 +12,42 @@
                     
                     <div class="grid lg:grid-cols-3 xl:grid-cols-4">
                         <div>
-                            filters
+                            <div class="pb-4">
+                                <fieldset>
+                                    <legend class="w-full px-2">
+                                        <!-- Expand/collapse section button -->
+                                        <button type="button" class="w-full p-2 flex items-center justify-between text-gray-400 hover:text-gray-500" aria-controls="filter-section-0" aria-expanded="false">
+                                            <span class="text-sm font-medium text-gray-900">
+                                                Category
+                                            </span>
+                                            <span class="ml-6 h-7 flex items-center">
+                                                <!--
+                                                Expand/collapse icon, toggle classes based on section open state.
+                            
+                                                Heroicon name: solid/chevron-down
+                            
+                                                Open: "-rotate-180", Closed: "rotate-0"
+                                                -->
+                                                <svg class="rotate-0 h-5 w-5 transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                </svg>
+                                            </span>
+                                        </button>
+                                    </legend>
+                                    <div class="pt-4 pb-2 px-4" id="filter-section-0">
+                                        <div class="space-y-6">
+                                            @foreach ($categories as $category)
+                                                <div class="flex items-center">
+                                                    <input id="color-0-mobile" name="color[]" value="white" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
+                                                    <label for="color-0-mobile" class="ml-3 text-sm text-gray-500">
+                                                        {{ $category->name }}
+                                                    </label>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </div>
                         </div>
                         <div class="lg:col-span-2 xl:col-span-3">
                             <div class="max-w-7xl mx-auto overflow-hidden sm:px-6 lg:px-8">
